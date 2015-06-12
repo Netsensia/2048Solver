5d66bcf72d4b106731ef948d266130512f27ba0f
Chris Moreton <chris@netsensia.com>
2015-06-12 19:04:17 +0100

    Default to two threads

--- src/com/netsensia/twentyfortyeight/TwentyFortyEight.java
+++ src/com/netsensia/twentyfortyeight/TwentyFortyEight.java
@@ -2,11 +2,12 @@ package com.netsensia.twentyfortyeight;
 
 public class TwentyFortyEight {
 
-	public static final int DEPTH = 6;
+	public static final int DEPTH = 4;
 	
-	// Set this to the number of CPU cores
-	// Any higher than that and no additional benefit is likely
-	public static final int NUM_THREADS = 4;
+	// Two threads appears to be optimal. Even on a true 6 core
+	// machine, the code can end up being slower with more than
+	// two threads.
+	public static final int NUM_THREADS = 2;
 	
 	public static final int RUNS = 1000;
     public static final int POWER_MAX = 32;
