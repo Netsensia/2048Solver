089684ad81b4e3d40890cfa646095517a87d11b9
Chris Moreton <chris@netsensia.com>
2015-06-12 18:54:01 +0100

    Default to depth 6

--- src/com/netsensia/twentyfortyeight/TwentyFortyEight.java
+++ src/com/netsensia/twentyfortyeight/TwentyFortyEight.java
@@ -2,8 +2,12 @@ package com.netsensia.twentyfortyeight;
 
 public class TwentyFortyEight {
 
-	public static final int DEPTH = 5;
-
+	public static final int DEPTH = 6;
+	
+	// Set this to the number of CPU cores
+	// Any higher than that and no additional benefit is likely
+	public static final int NUM_THREADS = 4;
+	
 	public static final int RUNS = 1000;
     public static final int POWER_MAX = 32;
 	
@@ -16,7 +20,7 @@ public class TwentyFortyEight {
 		int numCores = Runtime.getRuntime().availableProcessors();;
 		System.out.println("Number of available cores: " + numCores);
 		
-		for (int i=0; i<numCores; i++) {
+		for (int i=0; i<NUM_THREADS; i++) {
 			new Thread(new GameRunner(resultsLogger, DEPTH)).start();
 		}
 
