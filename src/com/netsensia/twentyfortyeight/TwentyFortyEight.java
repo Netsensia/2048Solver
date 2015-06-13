74d2036d16d77f0ff25eaf64d5b2f92063df9c26
Chris Moreton <chris@netsensia.com>
2015-06-13 02:18:00 +0100

    Results for depth 8

--- src/com/netsensia/twentyfortyeight/TwentyFortyEight.java
+++ src/com/netsensia/twentyfortyeight/TwentyFortyEight.java
@@ -2,10 +2,7 @@ package com.netsensia.twentyfortyeight;
 
 public class TwentyFortyEight {
 
-	public static final int DEPTH = 5;
-	
-	// A good value for this would be the number of CPU cores
-	public static final int NUM_THREADS = 12;
+	public static final int DEPTH = 8;
 	
 	public static final int RUNS = 1000;
     public static final int POWER_MAX = 32;
@@ -19,7 +16,7 @@ public class TwentyFortyEight {
 		int numCores = Runtime.getRuntime().availableProcessors();;
 		System.out.println("Number of available cores: " + numCores);
 		
-		for (int i=0; i<NUM_THREADS; i++) {
+		for (int i=0; i<numCores; i++) {
 			new Thread(new GameRunner(resultsLogger, DEPTH)).start();
 		}
 
